3cebda1282967aeac7ada9bddc3e5ff37ff5c45e
Soumeet Acharya <soumeet@Soumeets-MacBook-Air.local>
2025-04-21 11:50:36 +0530

    configuring api to trigger lambda which triggers the deployment

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -77,3 +77,72 @@ resource "aws_cloudwatch_event_target" "lambda_target" {
   arn  = aws_lambda_function.state_cleanup.arn
 }
 
+resource "aws_iam_role" "manual_trigger_lambda" {
+  name = "manual-trigger-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "manual_trigger_logging" {
+  role       = aws_iam_role.manual_trigger_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "manual_trigger" {
+  function_name = "manual-trigger"
+  handler       = "manual_trigger_lambda.lambda_handler"
+  runtime       = "python3.8"
+
+  filename      = "github_trigger.zip"
+  source_code_hash = filebase64sha256("github_trigger.zip")
+
+  environment {
+    variables = {
+      GITHUB_TOKEN = var.github_token
+    }
+  }
+
+  role = aws_iam_role.manual_trigger_lambda.arn
+}
+
+resource "aws_apigatewayv2_api" "manual_trigger" {
+  name          = "manual-trigger-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "manual_trigger" {
+  api_id                 = aws_apigatewayv2_api.manual_trigger.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.manual_trigger.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "manual_trigger" {
+  api_id    = aws_apigatewayv2_api.manual_trigger.id
+  route_key = "GET /trigger"
+  target    = "integrations/${aws_apigatewayv2_integration.manual_trigger.id}"
+}
+
+resource "aws_apigatewayv2_stage" "manual_trigger" {
+  api_id      = aws_apigatewayv2_api.manual_trigger.id
+  name        = "prod"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.manual_trigger.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.manual_trigger.execution_arn}/*/*"
+}
